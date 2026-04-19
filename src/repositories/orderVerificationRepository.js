a0f130c342e2df6eb2d73e00682bcd430349ff68
Krprasa <krishna_prasad@gap.com>
2026-04-18 19:52:03 -0700

    SMS Integration endpoints and updated DB constraints, update-10

--- src/repositories/orderVerificationRepository.js
+++ src/repositories/orderVerificationRepository.js
@@ -51,7 +51,7 @@ async function createVerificationSession(fields) {
       resend_available_at,
       verified_at
     )
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
     RETURNING *;
   `;
   const values = [
